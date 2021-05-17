c2918c3500631558980d70a7152e0175a70dbd6c
Peter Eang <peang@mail.greenriver.edu>
2021-05-16 23:51:58 -0400

    added model folder along with changes

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ session_start();
 
 //Require necessary files
 require_once ('vendor/autoload.php');
+require_once ('model/data-layer.php');
 
 //Instantiate Fat-Free
 $f3 = Base::instance();
@@ -22,3 +23,44 @@ $f3->route('GET /', function(){
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
+$f3->route('GET|POST /survey', function ($f3){
+
+    //Reinitialize a session array
+    $userChoices = array();
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //If the form has ben submitted, validated the data
+
+        if (!empty($_POST['choices'])) {
+
+            //Get user input
+            $userChoices = $_POST['choices'];
+
+            //validation here
+            if (validChoice($userChoices)) {
+                $_SESSION['choices'] = implode(", ", $userChoices);
+            }
+            else {
+                $f3->set('errors["choices"]', 'Invalid selection');
+            }
+        }
+
+        if (empty($f3->get('errors'))) {
+            header('location: summary');
+        }
+    }
+
+    //Get the data from the model
+    $f3->set('choices', getChoices());
+
+    //Store the user input in the hive
+    $f3->set('userChoice', $userChoices);
+
+    //Display the Survey Form
+    $view = new Template();
+    echo $view->render('views/survey.html');
+});
+
+//Run Fat-Free
+$f3->run();
---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+
+/*  data-layer.php
+ *  Return data for the midterm survey
+ *
+ */
+
+//Get the choices made by the user
+function getChoices()
+{
+    return array('This midterm is easy', 'This midterm is okay', 'This midterm is hard man!');
+}
